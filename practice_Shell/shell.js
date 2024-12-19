34a0f8f366dbe825e17e7ca39e71cee39ed01b29
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2024-12-20 01:25:10 +0530

    shell file added with function echo,cd,pwd,touch,mkdir

---
+++ practice_Shell/shell.js
@@ -0,0 +1,87 @@
+let PS1 = "Jarvis ";
+const home = PS1;
+const files = [];
+const directories = [];
+
+const getComment = function (comment) {
+  const array = comment.split("/");
+  array.pop();
+  if (array[0] === undefined) {
+    return comment;
+  }
+  return array.join("/");
+}
+
+const getFiles = function (arg) {
+  const existingFiles = files.concat(directories).flat();
+  const target = arg.toString();
+  if (existingFiles.join("/").includes(target)) {
+    PS1 = PS1 + "/" + target;
+  } else {
+    return "no such files or directories found";
+  }
+}
+
+const cd = function (arg) {
+  if (arg.length === 0 || arg[0] === "") {
+    PS1 = home;
+  } else if (arg[0] === "..") {
+    PS1 = getComment(PS1);
+  } else {
+    return getFiles(arg);
+  }
+}
+
+const display = function () {
+  return (files.join(" ") + " " + directories.join(" "));
+}
+
+const createDirectories = function (name) {
+  if (name.length === 0 || name[0] === "") {
+    return;
+  }
+  directories.push(name);
+}
+
+const createFiles = function (name) {
+  if (name.length === 0 || name[0] === "") {
+    return;
+  }
+  files.push(name);
+}
+
+const notFound = function (command) {
+  return ("jarvis: no such command found : " + command);
+}
+
+const externalCommand = function (command, arg) {
+  const list = [["ls", display], ["cd", cd], ["mkdir", createDirectories], ["touch", createFiles]];
+  const choice = list.filter(function (x) { return x[0] === command }).flat();
+  if (choice.length !== 0) {
+    return choice[1](arg);
+  }
+  return notFound(command);
+}
+
+const runCommand = function (command, arg) {
+  if (command.length === 0) {
+    return;
+  }
+  switch (command) {
+    case "echo":
+      return arg.join(" ");
+    case "pwd":
+      return PS1;
+    default:
+      return externalCommand(command, arg);
+  }
+}
+
+while (true) {
+  const input = prompt(PS1 + " ~ %");
+  const [command, ...arg] = input.split(" ");
+  const result = runCommand(command, arg);
+  if (result !== undefined) {
+    console.log(result);
+  }
+}
